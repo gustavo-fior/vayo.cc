4370f0b45a213ebc6025054cc926141f4883f731
Gustavo <gustavo.fior@sbcash.com.br>
2023-08-04 18:35:40 -0300

    first form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,11 @@ import { api } from "~/utils/api";
 
 export default function Home() {
 
+  function handleBookmark(url: string) {
+    
+    console.log(url)
+  }
+
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[black] to-[#333131]">
@@ -10,6 +15,26 @@ export default function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Bookmarks
           </h1>
+          <form onSubmit={(e) => {
+            e.preventDefault()
+            handleBookmark(e.target.url.value)
+          }}>
+            <div className="flex flex-col items-center gap-4">
+              <input
+                type="text"
+                name="url"
+                id="url"
+                placeholder="https://example.com"
+                className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+              />
+              <button
+                type="submit"
+                className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+              >
+                Bookmark
+              </button>
+            </div>
+          </form>
           <div className="flex flex-col items-center gap-2">
             <AuthShowcase />
           </div>
