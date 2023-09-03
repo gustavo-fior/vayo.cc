8e5431387ec2f3f2355b1fa1510c003392427fd0
Gustavo <gustavo.fior@outlook.com>
2023-09-02 22:05:09 -0300

    fix more shit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,10 @@ import { getSession, signIn } from "next-auth/react";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 
 export default function Home() {
+  const handleSignIn = (provider: string) => {
+    signIn(provider).catch((err) => console.error(err));
+  };
+
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#1a1a1a] to-[black]">
@@ -10,23 +14,23 @@ export default function Home() {
           <h1 className="text-5xl font-extrabold  text-white sm:text-[5rem]">
             Bookmarks
           </h1>
-          <p className="text-lg text-slate-400 pt-8 font-semibold">
+          <p className="pt-8 text-lg font-semibold text-slate-400">
             Simple bookmark manager with a focus on privacy.
           </p>
-          <p className="text-lg text-slate-400 pt-3 italic font-semibold">
+          <p className="pt-3 text-lg font-semibold italic text-slate-400">
             No tracking, no ads, no analytics.
           </p>
           <div className="flex items-center gap-2 pt-8">
             <button
               className="flex items-center gap-2 rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-              onClick={() => signIn("google")}
+              onClick={() => handleSignIn("google")}
             >
               <p>Sign in with</p>
               <BsGoogle size={16} />
             </button>
             <button
               className="flex items-center gap-2 rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-              onClick={() => signIn("github")}
+              onClick={() => handleSignIn("github")}
             >
               <p>Sign in with</p>
               <BsGithub size={16} />
