f814fdbb0b1e105979682f01b5c63c6cf7abf191
Gustavo <gustavo.fior@sbcash.com.br>
2023-11-01 16:30:53 -0300

    fix typos in privacy policy

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -15,23 +16,23 @@ export default function Home() {
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 5 }}
           transition={{ duration: 0.3 }}
-          className="flex flex-col justify-center px-4 py-16 w-full max-w-[60rem]"
+          className="flex w-full max-w-[50rem] flex-col justify-center px-4 py-16"
         >
           <h2 className="pt-4 text-3xl font-bold text-slate-100 transition duration-500">
             Privacy Policy
           </h2>
           <p className=" py-3  text-slate-400 transition duration-500">
-            This Privacy Policy describes how Bookmarks collects, uses, and discloses personal information when you
-            use the Bookmarks Chrome extension (the
-            &quot;service&quot;).
+            This Privacy Policy describes how Bookmarks collects, uses, and
+            discloses personal information when you use the Bookmarks Chrome
+            extension (the &quot;service&quot;).
           </p>
           <h2 className="pt-4 text-xl font-bold text-slate-100 transition duration-500">
             Information Collection and Use
           </h2>
           <p className=" py-3  text-slate-400 transition duration-500">
             The service does not collect any personal information from its
-            users. We simply fetch your current session from the Chrome API (cookies) and 
-            use it to fetch your folders and save your bookmarks.
+            users. We simply fetch your current session from the Chrome API
+            (cookies) and use it to fetch your folders and save your bookmarks.
           </p>
           <h2 className="pt-4 text-xl font-bold text-slate-100 transition duration-500">
             Data Security
@@ -58,7 +59,10 @@ export default function Home() {
           </h2>
           <p className=" py-3  text-slate-400 transition duration-500">
             If you have any questions or suggestions about our Privacy Policy,
-            do not hesitate to contact us at [Your Contact Information].
+            do not hesitate to contact us at the{" "}
+            <Link className="text-white hover:text-slate-300 trasition duration-300" href={"https://x.com/heyimgustavo"}>
+              developer&apos;s Twitter account.
+            </Link>
           </p>
         </motion.div>
       </main>
