53423b4b8a22e957553a240aaa6734a6f13cb805
Gustavo <gustavo_fior@outlook.com>
2024-01-22 23:29:29 -0300

    update speed analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import "@radix-ui/themes/styles.css";
-import { Analytics } from "@vercel/analytics/react";
 import "inter-ui/inter.css";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
@@ -7,6 +6,7 @@ import "~/styles/globals.css";
 import { api } from "~/utils/api";
 import { ThemeProvider } from "next-themes";
 import { type Session } from "next-auth";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -15,9 +15,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <ThemeProvider attribute="class" disableTransitionOnChange>
-          {/* <CssBaseline /> */}
-          <Component {...pageProps} />
-          <Analytics />
+        {/* <CssBaseline /> */}
+        <Component {...pageProps} />
+        <SpeedInsights />
       </ThemeProvider>
     </SessionProvider>
   );
